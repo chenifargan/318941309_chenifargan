687b206ecfaf936276c5b2bcd4459e0c3e91a171
chenifargan <chenifargan12@gmail.com>
2022-05-23 20:07:27 +0300

    Initial Commit

---
+++ MovieFinalproject_318941309/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieFinalproject_318941309
+//
+//  Created by חן איפרגן on 23/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
